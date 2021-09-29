17b1f62e658fe66441d2336e1c91ebdc40fe98b3
2pichar <charles.ludden@gmail.com>
2021-09-29 21:39:50 +0000

    Added links to get list of stocks

--- Main.ts
+++ Main.ts
@@ -12,6 +12,10 @@ var Periods =  {
   Year_1:'Year_1',
   Year_2:'Year_2'
 }
+
+const ndxlsturl = "https://pkgstore.datahub.io/core/nasdaq-listings/nasdaq-listed-symbols_csv/data/595a1f263719c09a8a0b4a64f17112c6/nasdaq-listed-symbols_csv.csv";
+const nyselsturl = "https://pkgstore.datahub.io/core/nyse-other-listings/nyse-listed_csv/data/3c88fab8ec158c3cd55145243fe5fcdf/nyse-listed_csv.csv";
+
 const msPerDay = 1000*60*60*24;
 
 const urlFetch = UrlFetchApp;
