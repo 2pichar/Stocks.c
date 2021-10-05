b18ea693af10c1e2fd4f789b232e2af6652f7eea
2pichar <charles.ludden@gmail.com>
2021-10-05 16:26:09 +0000

    Fixed indentation

--- node/utils.ts
+++ node/utils.ts
@@ -1,16 +1,16 @@
 const parseCsv = function(csv: str): str[][]{
-    let lines: str[] = csv.split('\n');
-    let line: str;
-    let data: str[][] = [];
-    for(line of lines){
-        let items = line.split(',');
-        data.push(items);
-    }
-    return data;
+	let lines: str[] = csv.split('\n');
+	let line: str;
+	let data: str[][] = [];
+	for(line of lines){
+		let items = line.split(',');
+		data.push(items);
+	}
+	return data;
 }
 const avg = function(...nums: int[]): int{
-    let res = nums.reduce((prev, acc)=>prev+acc);
-    res /= nums.length;
-    return res;
+	let res = nums.reduce((prev, acc)=>prev+acc);
+	res /= nums.length;
+	return res;
 }
 export {parseCsv, avg};
