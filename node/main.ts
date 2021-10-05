d7ebd2837696b18c9d10f9cfd1c864348433ed6f
2pichar <charles.ludden@gmail.com>
2021-10-05 16:34:43 +0000

    Fixed indentation

--- node/main.ts
+++ node/main.ts
@@ -3,51 +3,54 @@ import * as http from 'http'
 import * as request from './request';
 import * as fs from 'fs'
 
-const webLoc = './../web/'
+const webLoc = './../web'
 
 const server = http.createServer();
 server.on('request', (req, res)=>{
-		var path: str = req.url ?? '/';
-		var url: URL = new URL(path, req.headers.host);
-		var body: str = '';
-		var code: int = 200;
-		var msg: str = request.Status[200];
-		var type: str = 'text/html';
-		if (path.endsWith('.html')) {
-				try{
-						body = fs.readFileSync(`${webLoc}html${path}`, 'utf-8');
-				} catch(e) {
-						code = 404;
-						msg = request.Status[404];
-				}
+	var path: str = req.url ?? '/';
+	var url: URL = new URL(path, req.headers.host);
+	var method: str = req.method;
+	var body: str = '';
+	var code: int = 200;
+	var msg: str = request.Status[200];
+	var type: str = 'text/html';
+	if (path.endsWith('.html')) {
+		try{
+				body = fs.readFileSync(`${webLoc}/html${path}`, 'utf-8');
+		} catch(e) {
+			code = 404;
+			msg = request.Status[404];
 		}
-		else if (path.endsWith('.js')) {
-				try {
-					body = fs.readFileSync(`${webLoc}js${path}`, 'utf-8');
-					type = 'text/javascript';
-				} catch(e) {
-					code = 404;
-					msg = request.Status[404];
-				}
+	}
+	else if (path.endsWith('.js')) {
+		try {
+			body = fs.readFileSync(`${webLoc}/js${path}`, 'utf-8');
+			type = 'text/javascript';
+		} catch(e) {
+			code = 404;
+			msg = request.Status[404];
 		}
-		else if (path.endsWith('.css')) {
-			try {
-				body = fs.readFileSync(`${webLoc}css${path}`, 'utf-8');
-				type = 'text/css';
-			} catch(e) {
-				code = 404;
-				msg = request.Status[404];
-			}
+	}
+	else if (path.endsWith('.css')) {
+		try {
+			body = fs.readFileSync(`${webLoc}/css${path}`, 'utf-8');
+			type = 'text/css';
+		} catch(e) {
+			code = 404;
+			msg = request.Status[404];
 		}
-		if (path == '/') {
-				body = fs.readFileSync(`${webLoc}html/index.html`, 'utf-8');
-		}
-		else if (path == '/')
-		
-		res.writeHead(code, msg, {
-				'Content-Length':Buffer.byteLength(body),
-				'Content-Type': `text/${type}`
-		})
-		res.write(body);
-		res.end();
+	}
+	if (path == '/') {
+		body = fs.readFileSync(`${webLoc}/html/index.html`, 'utf-8');
+	}
+	else if (path == '/picks' || path == '/stockpicks') {
+		body = fs.readFileSync(`${webLoc}/html/stockpicks.html`, 'utf-8');
+	}
+	
+	res.writeHead(code, msg, {
+			'Content-Length':Buffer.byteLength(body),
+			'Content-Type': `text/${type}`
+	})
+	res.write(body);
+	res.end();
 });
