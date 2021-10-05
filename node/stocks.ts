6c75e6f389138b18e6bab5e04cd001803e6cb4bd
2pichar <charles.ludden@gmail.com>
2021-10-05 20:23:36 +0000

    updated imports for stocks.ts

--- node/stocks.ts
+++ node/stocks.ts
@@ -1,6 +1,6 @@
-import './types' // get types
-import request from './request';
-import * as util from './utils';
+import 'types' // get types
+import request from 'request';
+import * as util from 'utils';
 var getStockData = async function(ticker: str = "", type:str = "price"){
   var url: str = `https://query1.finance.yahoo.com/v7/finance/download/${ticker}?interval=1d&events=current&includeAdjustedClose=true`;
   var csv: str = await request(url);
