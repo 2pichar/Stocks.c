b6b785f1369e72ef98e3f435403bb90485470d92
2pichar <77556662+2pichar@users.noreply.github.com>
2021-11-05 17:16:23 -0700

    removed unnecessary code from request.ts

--- src/request.ts
+++ src/request.ts
@@ -1,11 +1,4 @@
-import * as http from 'http' // Import HTTP module
-declare module 'http' {
-    interface IncomingMessage {
-        rawBody: str;
-        body: strObj;
-    }
-}
-
+import * as http from 'http';
 async function request(url: str): Promise<str> {
     return new Promise((resolve, reject)=>{
         const request = http.get(url, (res)=>{
@@ -29,45 +22,7 @@ async function request(url: str): Promise<str> {
         request.end();
     });
 }
-async function getBody(req: http.IncomingMessage): Promise<str>{
-    return new Promise((resolve, reject)=>{
-        let data = []
-        req.on('data', (chunk)=>{
-            data.push(chunk)
-        })
-        .on('error', (err)=>{
-            reject(err);
-        })
-        .on('end', ()=>{
-            let rawBody = Buffer.concat(data).toString()
-            req.rawBody = rawBody;
-            if(rawBody != ''){
-                var query = rawBody.split('&');
-                var items: strObj = {};
-                for(var i of query){
-                    var [key, value] = i.split('=');
-                    items[key] = value;
-                }
-                req.body = items;
-            } else {
-                req.body = {};
-            }
-            resolve(rawBody);
-        });
-        
-    });
-}
 
-const Status: {[key: int]: str} = {
-    200: 'OK',
-    400: 'Bad Request',
-    401: 'Unauthorized',
-    404: 'Not Found',
-    405: 'Method Not Allowed',
-    501: 'Not Implemented'
-};
 export default request;
-export {request, getBody, Status};
-module.exports = {
-	request, Status, getBody
-};
+export {request};
+module.exports = request;
